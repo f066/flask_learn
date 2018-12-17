a39c8923c4019379a6b3f56b7d1aa470bf5ba4aa
gwf <gwf@gwfdeMacBook-Pro-4.local>
2018-12-17 14:00:28 +0800

    2.3 passed run options

--- hello.py
+++ hello.py
@@ -1,17 +1,23 @@
+#use "pip install flask-script" command before run this hello.py
+
 from flask import Flask
 from flask import request
+from flask_script import Manager
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>hello world!</h1><p> your browser is %s!</p>' % user_agent
+manager = Manager(app)
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>hello, %s!</h1>' % name
+#@app.route('/')
+#def index():
+#    user_agent = request.headers.get('User-Agent')
+#    return '<h1>hello world!</h1><p> your browser is %s!</p>' % user_agent
+#
+#@app.route('/user/<name>')
+#def user(name):
+#    return '<h1>hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
+    manager.run()
 
 

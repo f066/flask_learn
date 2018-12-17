51c00838efa08f4b34c0a0a8fded8a872bf6059e
gwf <gwf@gwfdeMacBook-Pro-4.local>
2018-12-17 12:49:26 +0800

    chapter2.5.1 get browser type

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>hello world! your browser is %s!</h1>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):

17c8572146e2c1029f73097025628b79086d74aa
f066 <1920266023@qq.com>
2019-01-11 12:41:06 +0800

    chapter 4.1 to 4.4

--- hello.py
+++ hello.py
@@ -1,20 +1,34 @@
-#use "pip install flask-bootstrap" command before run this hello.py
+#use "pip install flask-wtf" command before run this hello.py
 
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from flask import Flask,render_template
 #from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to gusess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 #manager = Manager(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('what is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('index.html', form=form, name=name)
 #    user_agent = request.headers.get('User-Agent')
 #    return '<h1>hello world!</h1><p> your browser is %s!</p>' % user_agent
 #

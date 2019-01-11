2ebeb161f77cf6030a5a5bcab3c1ff86e6bffd51
f066 <1920266023@qq.com>
2019-01-11 13:30:30 +0800

    4.6 显示flash提示信息

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask import Flask,render_template,session,redirect,url_for
+from flask import Flask,render_template,session,redirect,url_for,flash
 #from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -24,6 +24,9 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('look like you have change your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
 
